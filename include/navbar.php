0af1d62547fd24f019c6a3f4a9e0840994bfe00d
dayyumhey <dayyumhey@gmail.com>
2023-06-27 13:16:31 +0530

    first commit

---
+++ include/navbar.php
@@ -0,0 +1,49 @@
+<nav class="navigation-menu">
+  <div class="green-layer"></div>
+  <div class="black-layer"></div>
+  <div class="inner">
+    <ul>
+      <li><a href="#">Start</a>
+      	 <ul>
+          <li><a href="index.html">Slider</a></li>
+          <li><a href="index-video-bg.html">Video BG</a></li>
+          <li><a href="index-particles.html">Particles</a></li>
+          <li><a href="index-motion-blur.html">Motion Blur</a></li>
+          <li><a href="index-animation.html">Animation</a></li>
+        </ul>
+      </li>
+      <li><a href="unicord.html">Unicord</a></li>
+      <li><a href="#">Showcases</a>
+        <ul>
+          <li><a href="showcases-four-cols.html">4 Columns</a></li>
+          <li><a href="showcases-three-cols.html">3 Columns</a></li>
+          <li><a href="showcases-two-cols.html">2 Columns</a></li>
+        </ul>
+      </li>
+      <li><a href="news.html">News</a></li>
+      <li><a href="say-hello.html">Say Hello</a></li>
+    </ul>
+    <address>
+    Contornio Ave Kunsgatan 4<br>
+    41247 Luxembord<br>
+    074-753 5875
+    </address>
+  </div>
+  <!-- end inner --> 
+</nav>
+<!-- end navigation-menu -->
+  <nav class="navbar">
+    <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="Image"></a> </div>
+    <!-- end logo -->
+    <div class="email-us">Say <a href="#"><span>hello</span>@unicord.com</a></div>
+    <!-- end email-us -->
+    <div class="languages"> <a href="#" class="active">EN</a> <a href="#">RU</a> </div>
+    <!-- end languages -->
+    <div class="sandwich-nav"> <b>MENU</b>
+      <div class="sandwich-btn circle" id="sandwich-btn" data-dist="7"> <span></span> <span></span> </div>
+      <!-- end sandwich-btn --> 
+      
+    </div>
+    <!-- end sandwich-nav --> 
+  </nav>
+  
